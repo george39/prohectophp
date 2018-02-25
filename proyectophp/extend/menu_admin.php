93337e10282116a4cdb2af730710671c5bd2c7aa
george39 <george35@utp.edu.co>
2018-02-25 14:19:54 -0500

    inicio de proyecto

---
+++ proyectophp/extend/menu_admin.php
@@ -0,0 +1,19 @@
+<nav>
+	<a href="" data-activates="menu" class="button-collpase"><i class="material-icons">menu</i></a>
+</nav>
+
+<ul id="menu" class="side-nav fixed"> 
+	<li>
+		<div class="userView">
+			<div class="background">
+				<img src="https://i.ytimg.com/vi/FuHcna7q15U/maxresdefault.jpg">
+			</div> 
+			<a href="" ><img src="http://imagenesdecarrosdeportivos.org/wp-content/uploads/2017/10/imagenes-de-carros-deportivos-ferrari-rojo-e1508044077313.jpg" class="circle" alt=""></a>
+			<a href="" class="white-text">Jorge Ivan</a> <br>
+			<a href="" class="white-text">correo@correo.com</a>
+		</div>
+	</li>
+	<li><a href=""><i class="material-icons">home</i>Inicio</a></li>
+	
+	<li><div class="divider"></div></li>
+</ul>
---
+++ proyectophp/login/script.php
@@ -0,0 +1,9 @@
+</min>
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+<script src="../js/materialize.min.js"></script> 
+<script src="../cdn/sweetalert2.js"></script> 
+
+
